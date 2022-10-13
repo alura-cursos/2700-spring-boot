aa84a5ddc55ca1bd1b031b8aa8277fed4d0a5241
Rodrigo Caneppele <r.caneppele@gmail.com>
2022-10-13 11:53:04 -0300

    Nova migration

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -13,6 +13,9 @@ public record DadosCadastroMedico(
         @NotBlank
         @Email
         String email,
+
+        @NotBlank
+        String telefone,
         @NotBlank
         @Pattern(regexp = "\\d{4,6}")
         String crm,
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -19,6 +19,9 @@ public class Medico {
     private Long id;
     private String nome;
     private String email;
+
+    private String telefone;
+
     private String crm;
 
     @Enumerated(EnumType.STRING)
@@ -30,6 +33,7 @@ public class Medico {
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
+        this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
