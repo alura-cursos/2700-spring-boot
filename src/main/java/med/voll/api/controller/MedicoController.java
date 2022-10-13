4bad8470cd5ec3f7999998b81a5cec7f3fbc7fce
Rodrigo Caneppele <r.caneppele@gmail.com>
2022-10-12 21:52:26 -0300

    Recebendo dados na API

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,18 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody String json) {
+        System.out.println(json);
+    }
+
+
+}
