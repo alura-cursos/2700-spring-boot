06eb41d41bde690a6fcadf2ac9deb2f51ad479f4
Rodrigo Caneppele <r.caneppele@gmail.com>
2022-10-16 23:49:22 -0300

    Requisicoes DELETE

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -34,5 +34,11 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
     }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
 
 }
